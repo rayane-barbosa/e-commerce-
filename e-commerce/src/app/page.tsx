e8fd495281988c4af95dc1ce99912219d753ccd2
Rayane Barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-10-08 01:24:28 +0000

    [Avaliacao-2] feat: login page created using react-hook-form

---
+++ e-commerce/src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import Image from 'next/image'
+import styles from './page.module.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Register } from './pages/register/registerPage.tsx';
+import { Login } from './pages/login/loginPage';
+
+
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+      <div className={styles.description}>
+        
+        <Login />
+        <Register />
+        
+             </div>
+            
+    </main>
+  )
+}
---
+++ e-commerce/src/app/pages/login/loginPage.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import "./loginStyle.css";
+import { useForm, useFormContext } from "react-hook-form";
+
+export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Email"
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+        aria-invalid={errors.Email ? "true" : "false"}
+      />
+      {errors.Email && errors.Email.type === "required" && (
+        <span role="alert">Email is required</span>
+      )}
+      {errors.Email && errors.Email.type === "pattern" && (
+        <span role="alert">Invalid email format</span>
+      )}
+
+      <input
+        type="password"
+        placeholder="Password"
+        {...register("Password", {
+          required: true,
+          maxLength: 8,
+          minLength: 6,
+        })}
+        aria-invalid={errors.Password ? "true" : "false"}
+      />
+      {errors.Password && errors.Password.type === "required" && (
+        <span role="alert">Password is required</span>
+      )}
+      {errors.Password && errors.Password.type === "maxLength" && (
+        <span role="alert">Password is too long</span>
+      )}
+      {errors.Password && errors.Password.type === "minLength" && (
+        <span role="alert">Password is too short</span>
+      )}
+      <input type="submit" disabled={!!Object.keys(errors).length} />
+    </form>
+  );
+};

1558358473474ccd4760a9d88a5cfbcc1f3e5cc1
Rayane Barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-10-08 16:37:31 +0000

    [avaliacao-2] feat: register-errors-message

--- e-commerce/src/app/pages/register/registerPage.tsx
+++ e-commerce/src/app/pages/register/registerPage.tsx
@@ -3,14 +3,44 @@ import { register } from "module";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import '../../../../public/formsStyle.css'
+import { ErrorMessage } from "@/errorHandler/ErrorMessage";
+
+
+interface IFormRegisterValues {
+  Email: string;
+  Password: string;
+  MobileNumber: string;
+  cep: string;
+  endereco: string;
+  bairro: string;
+  numero: number;
+  complemento: string;
+  cidade: string;
+  estado: string;
+  PasswordValidation: string;
+  checkbox: boolean;
+}
+
 
 export const Register = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IFormRegisterValues>();
+
   const onSubmit = (data: any) => console.log(data);
+  console.log(errors);
+
+  const registerErrorMessages: Record<string, string> = {
+    required: "This field is required",
+    pattern: "Invalid format",
+    maxLength: "is too long",
+    minLength: "is too short",
+
+  }
+
+
   return (
     <form className="form-align"
     onSubmit={handleSubmit(onSubmit)}>
@@ -20,59 +50,113 @@ export const Register = () => {
         placeholder="Email"
         {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
       />
+
+      <ErrorMessage errorType={errors.Email?.type} messages={registerErrorMessages} />
+
       <input
         className="inputForms"
         type="tel"
         placeholder="Mobile number"
-        {...register("Mobile number", {
+        {...register("MobileNumber", {
           required: true,
+          pattern: /^\d+$/,
           minLength: 6,
           maxLength: 12,
         })}
       />
+      <ErrorMessage errorType={errors.MobileNumber?.type} messages={registerErrorMessages} />
 
       <input
         className="inputForms"
         type="text"
         placeholder="CEP"
-        {...register("cep")}
+        {...register("cep" ,{
+          required: true,
+          pattern: /^\d+$/,
+          minLength: 8,
+          maxLength: 8,
+        })}
       />
+
+      <ErrorMessage errorType={errors.cep?.type} messages={registerErrorMessages} />
+
       <input
         className="inputForms"
         type="text"
         placeholder="Rua"
-        {...register("endereco")}
+        {...register("endereco", {
+          required: true,
+          minLength: 3,
+          maxLength: 50,
+          })}
       />
+      <ErrorMessage errorType={errors.endereco?.type} messages={registerErrorMessages} />
+      
       <input
         className="inputForms"
         type="text"
         placeholder="Bairro"
-        {...register("bairro")}
+      {...register("bairro" ,{
+          required: true,
+          minLength: 3,
+          maxLength: 50,
+        })}
       />
+
+      <ErrorMessage errorType={errors.bairro?.type} messages={registerErrorMessages} />
+
       <input
         className="inputForms"
         type="text"
         placeholder="Número"
-        {...register("numero")}
+        {...register("numero", {
+          required: true,
+          minLength: 1,
+          maxLength: 10,
+        })}
       />
+
+      <ErrorMessage errorType={errors.numero?.type} messages={registerErrorMessages} />
+
       <input
         className="inputForms"
         type="text"
         placeholder="Complemento"
-        {...register("complemento")}
+        {...register("complemento", {
+          required: false,
+          minLength: 1,
+          maxLength: 50,
+        })}
       />
+
+      <ErrorMessage errorType={errors.complemento?.type} messages={registerErrorMessages} />
+
       <input
         className="inputForms"
         type="text"
         placeholder="Cidade"
-        {...register("cidade")}
+        {...register("cidade", {
+          required: true,
+          minLength: 3,
+          maxLength: 50,
+        })}
       />
+
+      <ErrorMessage errorType={errors.cidade?.type} messages={registerErrorMessages} />
+
       <input
         className="inputForms"
         type="text"
         placeholder="Estado"
-        {...register("estado")}
+        {...register("estado", {
+          required: true,
+          minLength: 2,
+          maxLength: 2,
+        })}
       />
+
+      <ErrorMessage errorType={errors.estado?.type} messages={registerErrorMessages} />
+
       <input
         type="password"
         placeholder="Password"
@@ -82,16 +166,24 @@ export const Register = () => {
           minLength: 6,
         })}
       />
+      <ErrorMessage errorType={errors.Password?.type} messages={registerErrorMessages} />
       <input
         type="password"
         placeholder="Password Validation"
-        {...register("Password Validation", {
+        {...register("PasswordValidation", {
           required: true,
           maxLength: 8,
           minLength: 6,
         })}
       />
-       <input type="checkbox" placeholder="Li e concondo com os termos de uso" {...register} />
+      <ErrorMessage errorType={errors.PasswordValidation?.type} messages={registerErrorMessages} />
+
+      <a>Li e concondo com os termos de uso <input type="checkbox" placeholder="" {...register("checkbox", {
+          required: true,
+        })} aria-invalid={!!errors.checkbox} />     
+       </a>
+      <ErrorMessage errorType={errors.checkbox?.type} messages={registerErrorMessages} />
+      
 
 
       <input type="submit"

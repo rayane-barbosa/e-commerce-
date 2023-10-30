31fe3eed340e27f6331413d80658679c6dc6152e
Rayane-Barbosa <rayane.c.barbosa@gmail.com>
2023-10-29 23:13:49 -0300

    [avaliacao-3] styles: update header and navigation components adding styles

--- e-commerce/src/app/components/header/header.tsx
+++ e-commerce/src/app/components/header/header.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import Navbar from "./navigationBar";
 import Image from "next/image";
 import logo from "../../../../public/logo.jpg";
+import * as Styled from "./header-styles";
 
 const altLogo = "Logo";
 
 export const Header = () => {
   return (
-    <header className="header">
-      <Image src={logo} alt={altLogo} width={100} height={100} />
-      <Navbar />
-      <hr />
-    </header>
+    <Styled.HeaderContainer>
+      <header>
+        <Image src={logo} alt={altLogo} width={100} height={100} />
+        <Styled.HeaderNavigation>
+          <nav>
+            <Navbar />
+          </nav>
+        </Styled.HeaderNavigation>
+      </header>
+    </Styled.HeaderContainer>
   );
 };
--- e-commerce/src/app/components/header/navigationBar.tsx
+++ e-commerce/src/app/components/header/navigationBar.tsx
@@ -2,35 +2,43 @@ import React from "react";
 import Link from "next/link";
 import { BsCart4 } from "react-icons/bs";
 import { VscAccount } from "react-icons/vsc";
-import { GrHomeRounded } from "react-icons/gr";
-
+import { HeaderNavigation } from "./header-styles";
 
 const Navbar = () => {
   return (
-    <>      
-        <div style={{ width: '100%', height: '30%', padding: 10, background: '#fff', justifyContent: 'center', alignItems: 'flex-start', gap: 10, display: 'inline-flex' }}>
-          <div style={{justifyContent: 'center',  alignItems: 'flex-start', gap: 24 , display: 'inline-flex' }}  >
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/home">
-                  <p> <GrHomeRounded /> Home </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/login">
-                  <p> <VscAccount /> My Account</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/cart">
-                  <p> <BsCart4 /> Cart </p>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      
-    </>
+    <HeaderNavigation>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/home">
+              <button> Home </button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <button> About </button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/myAccount">
+              <button>
+                {" "}
+                <VscAccount /> My Account{" "}
+              </button>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/cart">
+              <button>
+                {" "}
+                <BsCart4 /> Cart{" "}
+              </button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderNavigation>
   );
 };
 

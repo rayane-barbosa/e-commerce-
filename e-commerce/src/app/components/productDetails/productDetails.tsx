56b86288510383f59e0dd8a8c4e3f00cce320b74
Rayane-Barbosa <rayane.c.barbosa@gmail.com>
2023-10-29 22:45:53 -0300

    [avaliacao-3] update productDetails component with useState adding count

--- e-commerce/src/app/components/productDetails/productDetails.tsx
+++ e-commerce/src/app/components/productDetails/productDetails.tsx
@@ -1,39 +1,33 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { BsCartPlus } from "react-icons/bs";
+import { set } from "react-hook-form";
 
 interface IProductDetailsProps {
-    image: string;
-    name: string;
-    price: number;
+  image: string;
+  name: string;
+  price: number;
 }
 
 export const ProductDetails = (props: IProductDetailsProps) => {
-    const [item, setItem] = useState();
-    const [quantity, setQuantity] = useState(1);
+  let [cart, setCart] = useState([]);
+  let [count, setCount] = useState(0);
 
-    const handleAddToCart = () => {
-        const cartItem = {
-            item: setItem,
-            quantity: quantity,
-        };
-        console.log(cartItem);
-    };
+  const handleAddToCart = () => {
+    setCart([...cart, props.name, props.price, props.image]);
+    setCount(count + 1);
+    console.log(cart, count);
+  };
 
-    return (
-        <div>
-            <h3>{props.name}</h3>
-            <Image src={props.image} alt={props.name} width={200} height={200}/>
-            <p>${props.price}</p>
-            <BsCartPlus onClick={handleAddToCart}/> 
-                      
-
-
-            
-
-        </div>
-
-    );
-
-
-}
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <Image src={props.image} alt={props.name} width={200} height={200} />
+      <p>${props.price}</p>
+      <button onClick={handleAddToCart}>
+        <BsCartPlus /> Add to Cart
+      </button>
+      <p>{count}</p>
+    </div>
+  );
+};
